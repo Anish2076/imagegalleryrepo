a43eb39d7f8e55f0f37a0e1d094526ae69f15133
Anish2076 <anishjogi10@gmail.com>
2025-06-26 00:07:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 // linguist-language=JavaScript
-// Project Scripts
+console.log("Gallery script loaded");
+// Image loader function
+function loadImages() {
+  const gallery = document.querySelector('.gallery');
+  // Add image loading logic here
+  console.log("Images loaded");
+}
+// Event listeners
+window.addEventListener('DOMContentLoaded', () => {
+  loadImages();
+  // Add more functionality
+});
+// Add 5 more functions below
+function helper1() {}
+function helper2() {}
+// ...etc...
